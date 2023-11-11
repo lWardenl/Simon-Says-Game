05a45116ba94f04c8ba2623b35daaa50aacbc294
Warden <samer.charaf3@gmail.com>
2023-11-11 12:22:24 +0200

    Added the logic to work if a button is pressed and a reset level function

--- game.js
+++ game.js
@@ -3,13 +3,22 @@ const buttons = document.querySelectorAll('div[type="button"]');
 const colorPattern = [];
 const playerPattern = [];
 
+let level = 0;
 let isGameOver = false;
 
-console.log(buttons);
-
 function GameLoop() {
-  document.addEventListener("click", () => {
+  document.addEventListener("keydown", () => {
     AddNextColor();
+  });
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const color = event.target.id;
+      playerPattern.push(color);
+      console.log(playerPattern);
+      PlayAnimation(color);
+      PlayAudio(color);
+    });
   });
 }
 
@@ -17,19 +26,30 @@ function AddNextColor() {
   const randColor = Math.floor(Math.random() * 4);
   const color = colors[randColor];
   colorPattern.push(color);
-  console.log(color);
-  // PlayAnimation(color);
+  console.log(colorPattern);
+  PlayAnimation(color);
   PlayAudio(color);
 }
 
 function PlayAnimation(buttonID) {
-  // buttonID.classList.add("pressed");
+  const buttonPressed = document.getElementById(buttonID);
+  buttonPressed.classList.add("pressed");
+
+  setTimeout(() => {
+    buttonPressed.classList.remove("pressed");
+  }, 200);
 }
 
 function PlayAudio(color) {
   const soundFile = `./sounds/${color}.mp3`;
   const sound = new Audio(soundFile);
   sound.play();
+}
+
+function ResetGame() {
+  colorPattern = [];
+  playerPattern = [];
+  level = 0;
 }
 
 GameLoop();
